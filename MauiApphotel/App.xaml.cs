51e58b90a22993c86d275968416bdedd699f68f9
Silviamtt <silviamtt@gmail.com>
2025-11-13 21:20:44 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApphotel/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiApphotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new NavigationPage())
+            {
+                Width = 400,
+                Height = 600,
+            };
+
+            return window;
+        }
+    }
+}
